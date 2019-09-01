ddc7cb6ea4e5eba5275ef24476f2661db5d83349
Eduardo Ramirez <ramirezeduardo438@gmail.com>
2019-09-01 10:23:36 -0600

    Add project files.

---
+++ comercial/MasterPage/clientes.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/MasterPage/app.master" CodeBehind="clientes.aspx.vb" Inherits="comercial.clientes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
---
+++ comercial/conexiondb.vb
@@ -0,0 +1,36 @@
+﻿
+Imports System.Data.SqlClient
+Imports System.Data
+
+Public Class conexiondb
+    Private conexion As SqlConnection
+    Private cadenaconexion As String = "Data Source=PCGTUMG017\SQLEXPRESS; Initial Catalog=comercial; Integrated Security=True; encrypted=true; trustServerCertificate=true"
+
+
+    Public Sub New()
+        Me.conexion = New SqlConnection(Me.cadenaconexion)
+        Me.conexion.Open()
+    End Sub
+
+    Public Function ejconsulta(ByVal query As String)
+        Dim tabla As New DataTable
+        Dim adaptador As New SqlDataAdapter
+        Dim cmd As New SqlCommand
+
+        cmd.Connection = Me.conexion
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = query
+        adaptador.SelectCommand = cmd
+        adaptador.Fill(tabla)
+
+        If (Me.conexion.State = ConnectionState.Open) Then
+            Me.conexion.Close()
+        End If
+
+        Return tabla
+
+    End Function
+
+
+
+End Class
